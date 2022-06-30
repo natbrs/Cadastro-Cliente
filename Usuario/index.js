4a3a6a263ae0bf14bcf2724b082a1144e6b84e7b
natbrs <natanalmeidasp@gmail.com>
2022-06-30 11:31:56 -0300

    main

---
+++ Usuario/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, resp) {
+  resp.send("Bem-Vindo ao meu app");
+})
+
+app.get("/contato", function(req,resp){
+    resp.send("Página de contato do meu app");
+})
+
+app.get("/produto", function(req,resp){
+    resp.send("Página de contato do meu app");
+})
+
+app.get("/dados/:nome/:cargo", function(req,resp){
+    resp.send("<h1>Ola sr(a). "+req.params.nome+"</h1><h2>Seu cargo é"+req.params.cargo+"</h2>");
+})
+
+// esta sempre deve ser a ultima linha quando usamos o express
+app.listen(8081, function(err){
+    console.log("Servidor funcionando na url http://localhost:8081");
+})
+
+//npm install ejs 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs')
+var path = require('path');
+app.set('views', path.join(__dirname, '/view/'));
+
+// npm install consign 
+var consign = require('consign');
+const bodyParser = require("body-parser");
+consign().include('controller/routes').into(app);
+
+//npm install body-parser
+bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
